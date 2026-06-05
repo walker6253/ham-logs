a518c4e86f8ea35b4911719ab5d4c517efd9c6fe
ychen <ychen@quantangle.com.cn>
2026-06-05 23:39:45 +0800

    版本号 1.0.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.hamlog"
         minSdk = 26
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.0.10"
+        versionCode = 16
+        versionName = "1.0.12"
     }
 
     signingConfigs {
